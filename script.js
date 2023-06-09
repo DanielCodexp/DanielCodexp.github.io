dfa3b2999bdee2f50dab3056fe0782250b972607
gym-web <edgar.cortes1812@gmail.com>
2023-06-09 13:28:23 -0600

    Se agregaron cambios de datos

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
@@ -23,14 +25,14 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[2].classList.add("react");
+        habilidades[3].classList.add("jquery");
+        habilidades[4].classList.add("bootstrap");
+        habilidades[5].classList.add("angular");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("equipo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");
     }
 }
 
